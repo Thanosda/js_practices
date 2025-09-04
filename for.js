bf044905953f1ea336052273c9bf19704810abe1
thanosda <154046417+Thanosda@users.noreply.github.com>
2025-09-04 16:25:07 +0530

    Changes

---
+++ for.js
@@ -0,0 +1,3 @@
+for(let num = 1; num <= 13; num +=1) {
+    console.log(num);
+}
---
+++ less.js
@@ -0,0 +1,10 @@
+const pass=35
+var score=20
+
+if(pass<score){
+    console.log("you passed the exam")
+
+}
+else{
+    console.log("you failed the exam")
+}
